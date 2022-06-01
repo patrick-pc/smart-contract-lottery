2a332f1a7fb976def042144e9f71172ed37a1b34
spidΞy <web3slinger@gmail.com>
2022-06-01 10:12:23 -0500

    Initial commit

---
+++ hardhat.config.js
@@ -0,0 +1,10 @@
+require('@nomiclabs/hardhat-waffle')
+require('@nomiclabs/hardhat-etherscan')
+require('hardhat-deploy')
+require('hardhat-gas-reporter')
+require('solidity-coverage')
+require('dotenv').config()
+
+module.exports = {
+  solidity: '0.8.8',
+}
