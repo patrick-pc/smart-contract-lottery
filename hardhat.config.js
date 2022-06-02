61d354bef1e336af0e5051829667048badbc961c
spidΞy <web3slinger@gmail.com>
2022-06-02 10:04:27 -0500

    Build deploy scripts

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,6 +5,32 @@ require('hardhat-gas-reporter')
 require('solidity-coverage')
 require('dotenv').config()
 
+const PRIVATE_KEY = process.env.PRIVATE_KEY || '0xkey'
+const RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL || 'https://eth-rinkeby'
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || 'key'
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || 'key'
+
 module.exports = {
+  defaultNetwork: 'hardhat',
+  networks: {
+    hardhat: {
+      chainId: 31337,
+      blockConfirmations: 1,
+    },
+    rinkeby: {
+      chainId: 4,
+      blockConfirmations: 6,
+      url: RINKEBY_RPC_URL,
+      accounts: [PRIVATE_KEY],
+    },
+  },
   solidity: '0.8.8',
+  namedAccounts: {
+    deployer: {
+      default: 0,
+    },
+    player: {
+      default: 1,
+    },
+  },
 }
